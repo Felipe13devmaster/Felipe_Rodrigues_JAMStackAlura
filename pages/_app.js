0052388c27099d3c33b12b6cb652d3cb0612a28d
Felipe <felipe13rodrigues.fr@gmail.com>
2021-08-03 17:47:39 -0300

    Passei o primeiro lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
