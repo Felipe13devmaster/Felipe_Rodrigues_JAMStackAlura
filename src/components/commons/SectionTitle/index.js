1eb570ba8038cd4557671419a0e6c120b40fb3fa
Felipe <felipe13rodrigues.fr@gmail.com>
2021-08-05 12:19:51 -0300

    Configurei  o componnete SectionTitle.

--- src/components/commons/SectionTitle/index.js
+++ src/components/commons/SectionTitle/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
 
 const Titulo = styled.h2`
-  
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: normal;
+
+  ${breakpointsMedia({
+    xs: css`
+      font-size: 36px;
+      line-height: 43px;
+    `,
+    md: css`
+      font-size: 60px;
+      line-height: 72px;
+    `,
+  })}
 `;
 
 const SectionTitle = ({ children }) => (
   <Titulo>{children}</Titulo>
 );
+
+SectionTitle.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default SectionTitle;
