54c51cc74f2457b0117cb0a8127772db8a0521a3
Felipe <felipe13rodrigues.fr@gmail.com>
2021-08-06 16:49:13 -0300

    Arrumei o tamanho das imagens.

--- src/components/commons/CardDestaque/index.js
+++ src/components/commons/CardDestaque/index.js
@@ -1,33 +1,57 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
-import CardImage from '../CardImage';
 import CardTitle from '../CardTitle';
 import CardText from '../CardText';
 
 const ContentCardDestaque = styled.div`
   background-color: pink;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   
   ${breakpointsMedia({
     xs: css`
       width: 290px;
       height: 249px;
+      flex-direction: column;
     `,
     md: css`
       width: 900px;
+      height: 320px;
+      flex-direction: row;
+    `,
+  })}
+`;
+
+const CardImage = styled.img`
+ 
+  ${breakpointsMedia({
+    xs: css`
+      width: 288px;
+      height: 177px;
+    `,
+    md: css`
+      width: 593px;
       height: 320px;
     `,
   })}
 `;
 
+const CardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 307px;
+`;
+
 const CardDestaque = () => (
   <ContentCardDestaque>
-    <CardImage />
-    <CardTitle />
-    <CardText />
+    <CardImage alt="imagem" src="https://mariaeunicesousa.com/wp-content/uploads/2019/03/ecli-partial-solar-eclipse_20180810_600x320.jpg" />
+    <CardBody>
+      <CardTitle />
+      <CardText />
+    </CardBody>
   </ContentCardDestaque>
 );
 
