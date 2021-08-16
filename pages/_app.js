ac9567b1528495500995303103da369d946b970f
Felipe <felipe13rodrigues.fr@gmail.com>
2021-08-16 14:04:35 -0300

    Removi o favicon

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
