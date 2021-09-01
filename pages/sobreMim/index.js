f2f7140bd5aad06891e8d091cd970ab3118615fe
Felipe <felipe13rodrigues.fr@gmail.com>
2021-09-01 12:11:33 -0300

    Iniciei a construção da tela sobre mim.

--- pages/sobreMim/index.js
+++ pages/sobreMim/index.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
-const exPage2 = () => (
-  <div>Sobre Mim</div>
-);
+const SobreMimPage = ({ repositorios }) => {
+  console.log(repositorios);
+  return (
+    <div>
+      {repositorios.map((repositorio) => (
+        <li>{repositorio.name}</li>
+      ))}
+    </div>
+  );
+};
 
-export default exPage2;
+export async function getStaticProps() { // renderizando o conteudo estatico no servidor
+  const repositorios = await fetch('https://api.github.com/users/Felipe13devmaster/repos')
+    .then((res) => res.json());
+  return {
+    props: { repositorios },
+  };
+}
+
+export default SobreMimPage;
